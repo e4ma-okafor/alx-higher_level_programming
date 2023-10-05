860a0b5b9ae6d318210252a04b29f5161ab27467
e4ma-okafor <ifeomasandra13@gmail.com>
2023-10-04 22:01:54 -0700

    corrections

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+""" A module that copies a string"""
+
+
 def magic_string():
+    """Function that creates atring"""
+
+
     magic_string.counter = getattr(magic_string, 'counter', 0) + 1
