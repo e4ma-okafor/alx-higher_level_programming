0a36f9f3bc7c5cce5b6186b4cc6123f06cea6011
e4ma-okafor <ifeomasandra13@gmail.com>
2023-10-04 19:50:18 -0700

    a square is a rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,73 +1,52 @@
 #!/usr/bin/python3
-"""
-This is the "Rectangle"  module.
-
-This module provides a Rectangle class.
-"""
+"""creates class Rectangle"""
 
 
 class Rectangle:
-    """A Rectangle class with attributes width and height,
-    methods area, perimeter, print, str, repr, and del, and
-    class attribute number_of_instances that keeps track of # of instances,
-    class attribute print_symbol which is used as symbol for printing,
-    and static method bigger_or_equal that returns biggest rectangle.
-    """
+    """defines class Rectangle with private instance attributes width/height
+and public instance methods to return the rectangle area and primeter
+and public class attributes to track of number of instances and print symbol
+and can print the rectangle using print_symbol with print() or str()
+and returns representation of the rectangle to be used by eval()
+and prints message when deleted
+and static method to compare and return the largest rectangle based on area"""
 
     number_of_instances = 0
-    print_symbol = "#"
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
+        """instantiates class instance with optional width/height attributes"""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
-        return self.__width
+        """gets private instance attribute width"""
+        return (self.__width)
 
     @width.setter
     def width(self, value):
+        """sets private instance attribute width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
-        return self.__height
+        """gets private instance attribute height"""
+        return (self.__height)
 
     @height.setter
     def height(self, value):
+        """sets private instance attribute height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
         else:
-            return rect_2
+            self.__height = value
 
-    def __repr__(self):
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-
-    def __str__(self):
-        total = ""
-        for i in range(self.__height):
-            for j in range(self.__width):
-                try:
-                    total += str(self.print_symbol)
-                except Exception:
-                    total += type(self).print_symbol
-            if i is not self.__height - 1:
-                total += "\n"
-        return total
