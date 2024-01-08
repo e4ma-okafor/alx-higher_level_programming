e47eac4e40cb8bbf0a20a260655df7faf22289b8
e4ma-okafor <ifeomasandra13@gmail.com>
2024-01-07 16:29:42 -0800

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
-import urllib.request
-
-
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    import urllib.request as request
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        utf8_content = content.decode("UTF-8")
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(utf8_content))
